9420304aa31b637f973af53ac40b825b10a6a8da
Aarshi Chourasia <aarshi.1998@gmail.com>
2019-06-19 00:20:47 +0530

    Update Solution.cpp

--- Array/K-th element of two sorted Arrays/Solution.cpp
+++ Array/K-th element of two sorted Arrays/Solution.cpp
@@ -6,20 +6,20 @@ int main() {
 	int t;
 	cin>>t;
 	while(t--){
-	    long int n,m,k;
-	    cin>>n>>m>>k;
-	    long int a[n],b[m];
-	    int c[n+m]={0};
-	    for(long int i=0;i<n;i++){
-	        cin>>a[i];
-	        c[i]=a[i];
-	    }
-        for(long int i=0;i<m;i++){
-	        cin>>b[i];
-	        c[n+i]=b[i];
-        }
-        sort(c,c+n+m);
-        cout<<c[k-1]<<endl;
+		long int n,m,k;
+		cin>>n>>m>>k;
+		long int a[n],b[m];
+		int c[n+m]={0};
+		for(long int i=0;i<n;i++){
+			cin>>a[i];
+			c[i]=a[i];
+		}
+		for(long int i=0;i<m;i++){
+			cin>>b[i];
+			c[n+i]=b[i];
+		}
+		sort(c,c+n+m);
+		cout<<c[k-1]<<endl;
 	}
 	return 0;
 }
