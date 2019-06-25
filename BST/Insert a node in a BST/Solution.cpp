220bb754d9d1eae2cdff52b9e9b4d1efadac867a
aarshic <aarshi.1998@gmail.com>
2019-06-25 20:54:46 +0530

    Insert a node in a BST, Inorder Traversal and BST

---
+++ BST/Insert a node in a BST/Solution.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node {
+    int data;
+    Node* right;
+    Node* left;
+    
+    Node(int x){
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+Node* insert(Node* node, int data);
+void inorder(Node *root)
+{
+    if(root==NULL)
+        return;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node *root;
+        Node *tmp;
+        root = NULL;
+        int N;
+        cin>>N;
+        for(int i=0;i<N;i++)
+        {
+            int k;
+            cin>>k;
+            root = insert(root, k);
+        }
+        inorder(root);
+        cout<<endl;
+    }
+}
+/*This is a function problem.You only need to complete the function given below*/
+
+/* The structure of a BST node is as follows:
+struct Node {
+    int data;
+    Node* right;
+    Node* left;
+    
+    Node(int x){
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+}; */
+/* This function should insert a new node with 
+  given data and return root of the modified tree  */
+Node* insert(Node* node, int data){
+    // Your code here
+    if(node==NULL)
+        return new Node(data);
+    if(data==node->data)
+        return node;
+    if(data<node->data)
+        node->left=insert(node->left,data);
+    else
+        node->right=insert(node->right,data);
+}
+
